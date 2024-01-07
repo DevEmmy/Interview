44a07e80d93c65b1d794c9d577c6d0b507544f7c
DevEmmy <eolaosebikan60@gmail.com>
2024-01-07 23:20:06 +0100

    added all basic files

---
+++ backend/server.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import corsOptions from './src/config/cors';
+require("dotenv").config()
+import userRoute from "./src/routes/user-routes"
+
+const app = express();
+const port = String(process.env.PORT) || 3030;
+      
+// Set up your routes and middleware here
+app.use(cors(corsOptions));
+express.urlencoded({limit:"50mb", extended: false})
+express.json({limit:"50mb"})
+     
+// Run MongoDB
+mongoose.connect(process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/test-app`)
+const connection = mongoose.connection
+connection.once('open', ()=>{console.log('Database running Successfully')});
+      
+//render the html file
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use("/auth", userRoute)
+      
+// Run Server
+app.listen(port, () => {
+console.log(`Server running on port ${port}`);
+      
+  });
+        
